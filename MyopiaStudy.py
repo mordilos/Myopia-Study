7f762fb1086603e6701b27121fea811f3ba63779
neck <necktarios.black@gmail.com>
2018-02-07 15:07:03 +0200

    this update has accuracy acc: 89.71%

--- MyopiaStudy.py
+++ MyopiaStudy.py
@@ -18,23 +18,28 @@ np.random.seed(seed)
 dataset = np.loadtxt('myopia.csv',delimiter = ";",skiprows = 1)
 dataset = dataset[:,1:17]
 
+Y = dataset[:,1]
 X = np.delete(dataset,1,axis=1)
-Y = dataset[:,2]
+
 
 (X_train, X_test, Y_train, Y_test) = train_test_split(X, Y, test_size=0.33, random_state=seed)
 
 # create the model
 model = Sequential()
-model.add(Dense(15, input_dim=15, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
+model.add(Dense(10, input_dim=15, activation='relu'))
+model.add(Dense(6, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
 # compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=200, batch_size=5)
+model.fit(X_train, Y_train, nb_epoch=200, batch_size=10)#, validation_data=(X_test, Y_test)
 
 # evaluate the model
 scores = model.evaluate(X_test, Y_test)
-print ("Accuracy: %.2f%%" %(scores[1]*100))
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+
+
 
